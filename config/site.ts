cb05f0a9ef3d81b48136065c0df345ad51ec4fad
Nick Fanelli <nickfanelli5@gmail.com>
2025-01-01 12:24:34 -0500

    chore: add next js

---
+++ config/site.ts
@@ -0,0 +1,18 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "NJ Title Buddy",
+  description: "", // TODO
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+  ],
+  navMenuItems: [
+    
+  ],
+  links: {
+   
+  },
+};
